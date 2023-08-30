42a7d01db2e20bd39bc83233da2727302dac83cb
Brando Gutierrez <45102279+bgtz02@users.noreply.github.com>
2023-08-29 21:41:53 -0500

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,61 @@
+/*
+//Basics
+
+40 + 8 + 2 - 10;
+console.log(40 + 8 + 2 - 10);
+
+let firstName = "Brando";
+let number = 23;
+
+console.log(firstName);
+console.log(number);
+
+//Lecture 1
+
+let country = "United States";
+let continent = "North America";
+let population = 326000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//Lecture 2
+let isIsland = "United States";
+let language = "English";
+
+console.log(isIsland);
+console.log(language);
+*/
+
+//Data Types
+/*
+let javaScriptIsFun = false;
+console.log(javaScriptIsFun);
+
+
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof 'Brando');
+console.log(typeof javaScriptIsFun);
+
+javaScriptIsFun = 'YES';
+
+console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2002;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+//Mutate a Variable or declare an empty variable
+let age = 30;
+age = 31;
+
