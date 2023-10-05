7d92b5bb14db605a422a2e22456046132600cda6
Brando Gutierrez <45102279+bgtz02@users.noreply.github.com>
2023-10-05 13:46:33 -0500

    Math, Assginement, and Comparison operators
    code was added to test Math, Assignment, and Comparison Operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -70,9 +70,39 @@ console.log(typeof null);
 lastName = 'Texas';
 console.log(lastName);
 */
-
-
-const ageJonas = 2037 - 1991;
-const ageSarah = 2037 - 2018;
+//Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
 
 console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2** 3 means 2 to the power of 3 = 2*2*2
+
+const firstName = 'Jonas';
+const lastName = 'Texas';
+console.log(firstName + ' ' + lastName);
+
+//Asignment operators
+let x = 10 + 5; //15
+console.log(x);
+// after ^ is executed the stored value of x is now 15
+x += 10; // x = x + 10 = 25
+console.log(x);
+x *= 4; // x = x * 4 =100
+console.log(x);
+
+x++; //x = x + 1
+console.log(x);
+
+x--; //x = x -1 
+console.log(x);
+
+//Comperason Operators - booleans values
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
