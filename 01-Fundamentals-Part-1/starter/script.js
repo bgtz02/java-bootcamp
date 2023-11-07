11d6929627901efb1925e42465850c3ae5147f31
Brando Gutierrez <45102279+bgtz02@users.noreply.github.com>
2023-11-07 16:06:12 -0600

    Template Literals
    Using `` Back ticks to create Template Literals

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -107,15 +107,34 @@ console.log(lastName);
 
 // console.log(now - 1991 > now - 2018);
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
 
-console.log(now - 1991 > now - 2018);
+// console.log(now - 1991 > now - 2018);
 
-let x, y;
-x = y = 25 - 10 - 5;
-console.log(x, y);
+// let x, y;
+// x = y = 25 - 10 - 5;
+// console.log(x, y);
 
-const averageAge = (ageJonas + ageSarah) / 2
-console.log(ageJonas, ageSarah, averageAge);
+// const averageAge = (ageJonas + ageSarah) / 2
+// console.log(ageJonas, ageSarah, averageAge);
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!'
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName} a ${year - birthYear} year old ${job}! `;
+console.log(jonasNew);
+
+console.log(`Just a Stirng \n\
+mulitple \n\
+lines`);
+
+console.log(`string
+multiple
+lines`);
