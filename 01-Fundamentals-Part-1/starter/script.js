c8895dce942b96188f086b7d7c543c384f3f21e3
Brando Gutierrez <45102279+bgtz02@users.noreply.github.com>
2023-11-08 13:43:28 -0600

    If else statements
    If and else statements are used to understand how it works.
    
    Variable cannot be declared in a if else code block, but it can be changed. variable has to be created outside and defined within the statement

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -119,7 +119,7 @@ console.log(lastName);
 
 // const averageAge = (ageJonas + ageSarah) / 2
 // console.log(ageJonas, ageSarah, averageAge);
-
+/*
 const firstName = 'Jonas';
 const job = 'teacher';
 const birthYear = 1991;
@@ -139,3 +139,24 @@ lines`);
 console.log(`string
 multiple
 lines`);
+*/
+
+const age = 17;
+const isOldEnough = age >= 18;
+
+if (age >= 18) {
+    console.log(`Sarah can start driving lessons`);
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sara is too young wait ${yearsLeft} years`);
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
