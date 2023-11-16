5c2282c8b125114ea6ab7bf6d7a34b8e9757438a
Brando Gutierrez <45102279+bgtz02@users.noreply.github.com>
2023-11-16 15:24:47 -0600

    Added previous code to correct section

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,18 @@
+//Section 3 #32
+'use strict'; // activates strict mode will not work if code is above this
+
+let hasDriversLicence = false;
+const passTest = true;
+
+if (passTest) hasDriversLicence = true;
+if (hasDriversLicence) console.log(`I Can Drive`);
+
+/*
+Strict mode will reserve some words for future use in
+JavaScript and will be unusable while in Strictmode
+private, Interface, and if are restricted words
+*/
+
+// const private = 34;
+// const if = 23;
+
--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,17 +1,2 @@
-// Remember, we're gonna use strict mode in all scripts now!
+
 'use strict'; // activates strict mode will not work if code is above this
-
-let hasDriversLicence = false;
-const passTest = true;
-
-if (passTest) hasDriversLicence = true;
-if (hasDriversLicence) console.log(`I Can Drive`);
-
-/*
-Strict mode will reserve some words for future use in
-JavaScript and will be unusable while in Strictmode
-private, Interface, and if are restricted words
-*/
-
-// const private = 34;
-// const if = 23;
